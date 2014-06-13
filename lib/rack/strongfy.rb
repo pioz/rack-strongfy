5f635f7e11f5e5c90f3fdde5d1c69a2e4f852bf4
pioz <enrico@megiston.it>
2014-06-13 14:58:16 +0200

    parse html with nokogiri

--- lib/rack/strongfy.rb
+++ lib/rack/strongfy.rb
@@ -1,9 +1,11 @@
+require 'nokogiri'
+
 module Rack
   class Strongfy
     include Rack::Utils
 
     class << self
-      attr_accessor :word_list
+      attr_accessor :word_list, :selection_class
     end
 
     def initialize(app)
@@ -13,10 +15,15 @@ module Rack
     def call(env)
       status, headers, body = @app.call(env)
       if self.class.word_list.is_a?(Array)
+        classes = self.class.selection_class.is_a?(Array) ? self.class.selection_class.map{|x| ".#{x}"}.join(',') : '*'
         body.each do |b|
+          doc = Nokogiri::HTML(b)
           self.class.word_list.each do |word|
-            b.gsub!(/\b(#{word})\b/i, '<strong>\\1</strong>')
+            doc.css(classes).each do |elem|
+              elem.content = elem.content.gsub(/\b(#{word})\b/i, '<strong>\\1</strong>')
+            end
           end
+          b = doc.to_s
         end
       end
       [status, headers, body]
